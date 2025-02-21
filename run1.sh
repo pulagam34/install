5cdd4a01e4792fe212005d30ad2292d95bcc55b7
pulagam34 <discordshiva300+34@gmail.com>
2025-02-21 23:34:42 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -3,7 +3,7 @@
 /root/node-1/bin/gaianet info --base /root/node-1
 /root/node-1/bin/gaianet info --base /root/node-2
 while true; do
-    tput reset  # Resets the terminal screen
+    python -c "from IPython.display import clear_output; clear_output(wait=True)"
     nvidia-smi
     sleep 5
 done
