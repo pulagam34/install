1d120fabd6176a7b646741a0f3832012d919a082
pulagam34 <discordshiva300+34@gmail.com>
2025-02-21 23:52:30 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -3,11 +3,4 @@
 /root/node-1/bin/gaianet info --base /root/node-1
 /root/node-1/bin/gaianet info --base /root/node-2
 
-# Run the monitoring loop with nvidia-smi and clear output in Colab via Python
-while true; do
-    # Use Python to clear the output area in Colab
-    python -c "from IPython.display import clear_output; clear_output(wait=True)"
-    # Run nvidia-smi to show GPU status
-    nvidia-smi
-    sleep 5
-done
+wget https://raw.githubusercontent.com/pulagam34/install/main/clear.py -O /root/node-1/clear.py
