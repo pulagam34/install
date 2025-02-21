5f30dbb72eff6503be71694cec43c16967df6d2d
pulagam34 <discordshiva300+34@gmail.com>
2025-02-21 23:33:43 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -2,10 +2,8 @@
 #
 /root/node-1/bin/gaianet info --base /root/node-1
 /root/node-1/bin/gaianet info --base /root/node-2
-
-# Run the monitoring loop with nvidia-smi and clear output in Colab via Python
 while true; do
-    echo -e "\033c"  # Reset terminal (alternative to clear)
+    tput reset  # Resets the terminal screen
     nvidia-smi
     sleep 5
 done
