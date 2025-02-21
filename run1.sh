d5a8a574a10ef2edb85949ecce62b6cb48a63d8e
pulagam34 <discordshiva300+34@gmail.com>
2025-02-21 23:53:47 +0530

    Update run1.sh

--- run1.sh
+++ run1.sh
@@ -3,4 +3,4 @@
 /root/node-1/bin/gaianet info --base /root/node-1
 /root/node-1/bin/gaianet info --base /root/node-2
 
-wget https://raw.githubusercontent.com/pulagam34/install/main/clear.py -O /root/node-1/clear.py
+wget https://raw.githubusercontent.com/pulagam34/install/main/clear.py -O /root
